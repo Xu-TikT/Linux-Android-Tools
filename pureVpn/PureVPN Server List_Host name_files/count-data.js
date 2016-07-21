513c53cfd4524747a5db4fcc4ef570d7bbb73942
xwt <710323287@qq.com>
2016-07-21 16:32:42 +0800

    添加了一些ubuntu下的配置

--- pureVpn/PureVPN Server List_Host name_files/count-data.js
+++ pureVpn/PureVPN Server List_Host name_files/count-data.js
@@ -1,5 +1,5 @@
 var DISQUSWIDGETS;
 
 if (typeof DISQUSWIDGETS != 'undefined') {
-    DISQUSWIDGETS.displayCount({"text":{"and":"and","comments":{"zero":"0 Comments","multiple":"{num} Comments","one":"1 Comment"}},"counts":[{"id":"4963 https:\/\/support.purevpn.com\/?post_type=ht_kb&p=4963","comments":44}]});
+    DISQUSWIDGETS.displayCount({"text":{"and":"and","comments":{"zero":"0 Comments","multiple":"{num} Comments","one":"1 Comment"}},"counts":[{"id":"4963 https:\/\/support.purevpn.com\/?post_type=ht_kb&p=4963","comments":141}]});
 }
